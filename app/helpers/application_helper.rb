fda6ba9225e27a80873b5be4229f16e12ab721b2
William Rockwood <wrockwood@tadl.org>
2026-04-25 17:56:33 -0400

    Tighten legal and about page copy

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,20 @@
 module ApplicationHelper
+  LEGAL_EFFECTIVE_DATES = {
+    privacy: Date.new(2026, 4, 25),
+    terms: Date.new(2026, 4, 25)
+  }.freeze
+
   def build_label
     release = ENV["HUSHPAIR_RELEASE"].presence || ENV["RENDER_GIT_COMMIT"].presence || ENV["GIT_COMMIT_SHA"].presence
     release ||= local_git_revision
     return unless release.present?
 
     "build #{release.to_s.first(12)}"
+  end
+
+  def legal_effective_date(document)
+    date = LEGAL_EFFECTIVE_DATES.fetch(document.to_sym)
+    date.strftime("%B %-d, %Y")
   end
 
   private
